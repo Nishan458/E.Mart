1338cf347aed9d01c78336f28e1b0ee7be8fa350
Nishan458 <mdnishan283@gmail.com>
2026-06-10 12:36:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -389,3 +389,106 @@ function(){
     loadHomepageProducts();
 
 });
+/* =========================
+   ADMIN LOGIN
+========================= */
+
+const ADMIN_USERNAME = "nishan";
+const ADMIN_PASSWORD = "2016081";
+
+function adminLogin(){
+
+    const username =
+    document.getElementById(
+        "adminUsername"
+    ).value;
+
+    const password =
+    document.getElementById(
+        "adminPassword"
+    ).value;
+
+    if(
+        username === ADMIN_USERNAME &&
+        password === ADMIN_PASSWORD
+    ){
+
+        localStorage.setItem(
+            "adminLoggedIn",
+            "true"
+        );
+
+        showAdminDashboard();
+
+        alert(
+            "Admin Login Successful"
+        );
+
+    }else{
+
+        alert(
+            "Wrong Username or Password"
+        );
+    }
+}
+
+
+/* =========================
+   ADMIN LOGOUT
+========================= */
+
+function adminLogout(){
+
+    localStorage.removeItem(
+        "adminLoggedIn"
+    );
+
+    location.reload();
+}
+
+
+/* =========================
+   SHOW ADMIN DASHBOARD
+========================= */
+
+function showAdminDashboard(){
+
+    const loginSection =
+    document.getElementById(
+        "loginSection"
+    );
+
+    const dashboard =
+    document.getElementById(
+        "adminDashboard"
+    );
+
+    if(loginSection){
+        loginSection.style.display =
+        "none";
+    }
+
+    if(dashboard){
+        dashboard.style.display =
+        "block";
+    }
+}
+
+
+/* =========================
+   CHECK ADMIN LOGIN
+========================= */
+
+document.addEventListener(
+"DOMContentLoaded",
+function(){
+
+    if(
+        localStorage.getItem(
+            "adminLoggedIn"
+        ) === "true"
+    ){
+
+        showAdminDashboard();
+    }
+});
